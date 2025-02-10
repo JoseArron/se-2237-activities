533725c8caffd66b6c11072228d94ddbc89660c5
Jose <joseearronn@gmail.com>
2025-02-10 22:43:14 +0800

    [feat] add draggable block component

---
+++ src/components/ui/draggable-block.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+interface DraggableBlockProps {
+  color: string;
+  text?: string;
+  initialPosition?: { x: number; y: number };
+}
+
+export default function DraggableBlock({
+  color,
+  text = "DRAG ME!",
+  initialPosition = { x: 0, y: 0 },
+}: DraggableBlockProps) {
+  const [position, setPosition] = useState(initialPosition);
+  const [isDragging, setIsDragging] = useState(false);
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    setMousePos({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+    console.log(e.clientX, e.clientY);
+    console.log(position);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+    setPosition({
+      x: e.clientX - mousePos.x,
+      y: e.clientY - mousePos.y,
+    });
+    // console.log(e.clientX, e.clientY);
+  };
+
+  const handleMouseUp = () => setIsDragging(false);
+
+  return (
+    <div
+      className={`w-20 h-20 text-white flex items-center justify-center rounded-lg cursor-pointer absolute`}
+      style={{ left: position.x, top: position.y, backgroundColor: color }}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+    >
+      {isDragging ? "DRAGGING" : text}
+    </div>
+  );
+}
