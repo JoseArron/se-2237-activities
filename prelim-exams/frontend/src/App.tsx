db3ab331c92825f4d045c7def6f8ebad1aa8a972
Jose <joseearronn@gmail.com>
2025-03-04 20:20:07 +0800

    style: center tables

--- prelim-exams/frontend/src/App.tsx
+++ prelim-exams/frontend/src/App.tsx
@@ -9,7 +9,7 @@ export default  function App() {
   if (loading) return <div className="animate-spin" > Loading..</div>
 
   return (
-    <div className="flex sm:flex-row flex-col mx-4 min-h-screen gap-4 w-full">
+    <div className="flex sm:flex-row flex-col items-center mx-4 min-h-screen gap-4 w-full">
      <EmployeesTable data ={employees} label="All Employees"/>
      <EmployeesTable data ={employees.filter((employee) => employee.salary < 50000)} label="Entry Level" />
      <EmployeesTable data ={employees.filter((employee) => employee.salary >= 50000)} label="Senior"/>
