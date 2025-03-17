500b8562e626f96961b504a994f5249797d19ae5
Jose <joseearronn@gmail.com>
2025-03-17 21:46:38 +0800

    chore: initialize backend routes

---
+++ activity-3/backend/src/routes/students.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", async (req, res) => {});
+
+router.post("/", async (req, res) => {});
+
+router.put("/:id", async (req, res) => {});
+
+router.delete("/:id", async (req, res) => {});
+
+export default router;
--- activity-3/backend/src/server.ts
+++ activity-3/backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import studentsRouter from "./routes/students";
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/students", studentsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
