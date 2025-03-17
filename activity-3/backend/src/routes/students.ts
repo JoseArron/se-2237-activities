165dd3fa40d77e115fce12045afbfa0a046e7aa7
Jose <joseearronn@gmail.com>
2025-03-17 22:33:27 +0800

    feat: add student get and post routes

--- activity-3/backend/src/routes/students.ts
+++ activity-3/backend/src/routes/students.ts
@@ -1,10 +1,38 @@
 import { Router } from "express";
+import { prisma } from "../db/client";
+import { StudentInput } from "../types/student";
 
 const router = Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    prisma.students
+        .findMany()
+        .then((students) => {
+            res.status(200).json(students);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Failed to fetch students" });
+        })
+        .finally(() => res.status(200));
+});
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+    const data: StudentInput = req.body;
+    prisma.students
+        .create({
+            data: {
+                ...data,
+                // convert received date string to Date object
+                expectedDateOfDefense: new Date(data.expectedDateOfDefense),
+            },
+        })
+        .then((student) => {
+            res.status(201).json(student);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Failed to create student" });
+        });
+});
 
 router.put("/:id", async (req, res) => {});
 
---
+++ activity-3/backend/src/types/student.ts
@@ -0,0 +1,8 @@
+export interface StudentInput {
+    firstName: string;
+    lastName: string;
+    groupName: string;
+    role: string;
+    expectedSalary: number;
+    expectedDateOfDefense: Date;
+}
